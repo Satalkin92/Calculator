5162dede99dd00e4fd802227eace3e4214d2408d
Satalkin92 <satalkinkonstantin@gmail.com>
2022-12-04 17:55:46 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+
    public static void main (String[] args) throws IOException {
         System.out.println("Введите выражение:");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         Calc.calc(input);
+        scanner.close();
     }
 }
